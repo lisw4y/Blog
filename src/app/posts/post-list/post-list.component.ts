bbff1ab79bd6eec88536d1f0a345e91593a2b925
lisw4y <lsw99u@gmail.com>
2019-03-18 07:59:28 -0700

    Create a basic skeleton with Angular

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSubscription: Subscription;
+
+  constructor(private postsSerice: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsSerice.getPosts();
+    this.postsSubscription = this.postsSerice.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
+  }
+}
